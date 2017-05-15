07389e3514ff53a980fc2a068a25e22979f3a9c0
Michael Bechtel <elmiguelut99@gmail.com>
2017-05-15 07:36:05 -0400

    updates

--- bbdn/core/Validator.py
+++ bbdn/core/Validator.py
@@ -158,7 +158,7 @@ MembershipSchema = Schema({
     Optional("availability"): {
         Optional("available"): And(str, Use(str.title), lambda s: s in ['Yes', 'No'])
     },
-    Optional("courseRoleId"): And(str, lambda s: s in ["Instructor", "FacultyVC", "TeachingAssistant", "CourseBuilder",
+    Optional("courseRoleId"): And(str, lambda s: s in ["Instructor", "Faculty-VC", "TeachingAssistant", "CourseBuilder",
                                                        "Grader", "Student", "Guest"])
 })
 
--- settings.py
+++ settings.py
@@ -1,10 +1,10 @@
 PAGINATION = 10
 config = {
-    "key": "675372bb-4690-4e18-9744-ba72ab791a51",
-    "secret": "Ii0bPCLdnaPM6dPsiQcv91hTRRP1wS8T",
+    "key": "YOUR_KEY",
+    "secret": "YOUR_SECRET",
     "credentials": "client_credentials",
     "cert_path": "./trusted/keytool_crt.pem",
-    "target_url": "irscstaging.blackboard.com",
+    "target_url": "your.bb.instance",
     "api": {
         "base": {
             "path": "/learn/api/public/v1/"
